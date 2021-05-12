90a4ce3f5603e035905b0df1f095a63e978ebe80
Andy Haynes <andyghaynes@gmail.com>
2021-05-12 00:00:30 -0700

    better mash types

--- src/lib/types/mash.ts
+++ src/lib/types/mash.ts
@@ -1,19 +1,21 @@
-import { TemperatureMeasurement } from './measurement';
+import { TemperatureMeasurement, TimeMeasurement } from './measurement';
 
 export type MashProfile = {
   schedule: MashSchedule;
 };
 
 export type MashRest = {
+  type: RestType;
   temperature: TemperatureMeasurement;
-  minutes: number;
+  time: TimeMeasurement;
 };
 
 export type MashSchedule = {
   efficiency: number;
-  method: string;
+  method: MashMethod;
   rests: MashRest[];
-  sparge: string;
+  sparge: SpargeMethod;
+  recirculated: boolean;
 };
 
 export enum MashMethod {
@@ -26,4 +28,14 @@ export enum SpargeMethod {
   None = 'no sparge',
   Batch = 'batch sparge',
   Fly = 'fly sparge',
+}
+
+export enum RestType {
+  Acid = 'acid',
+  AlphaAmylase = 'alpha amylase',
+  BetaAmylase = 'beta amylase',
+  BetaGlucanase = 'beta glucanase',
+  Decoction = 'decoction',
+  FerulicAcid = 'ferulic acid',
+  Protease = 'protease',
 }
